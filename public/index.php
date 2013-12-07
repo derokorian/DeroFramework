c36fd819d603812f89e673c5aedae3324a5a9698
Ryan Pallas <ryan.pallas@gmail.com>
2013-12-06 20:01:07 -0700

    Created public folder
    Entry point, everything will be directed through index

---
+++ public/index.php
@@ -0,0 +1,25 @@
+<?php
+/**
+ * Single point of entry
+ * User: Ryan Pallas
+ * Date: 12/6/13
+ */
+define('ROOT', dirname(__DIR__));
+
+spl_autoload_register(function ($strClass)
+{
+    $strFile = $strClass . '.php';
+    $strNameSpace = '';
+    if ( ($iLast = strripos($strClass, '\\')) !== FALSE ) {
+        $strNameSpace = DIRECTORY_SEPARATOR . str_replace('\\',DIRECTORY_SEPARATOR,substr($strClass, 0, $iLast));
+        $strFile = substr($strClass, $iLast + 1) . '.php';
+    }
+    $strFilePath = ROOT . $strNameSpace . DIRECTORY_SEPARATOR . $strFile;
+    if( is_readable($strFilePath) ) {
+        require_once $strFilePath;
+        return TRUE;
+    }
+    return FALSE;
+});
+
+Dero\Core\Main::Init();
