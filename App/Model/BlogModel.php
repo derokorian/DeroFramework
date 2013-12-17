2d2a48bcdf6c4a0cf6225627f74146fb470722a4
Ryan Pallas <ryan.pallas@gmail.com>
2013-12-16 21:50:24 -0700

    Blog table definition

--- App/Model/BlogModel.php
+++ App/Model/BlogModel.php
@@ -9,13 +9,60 @@
  */
 namespace App\Model;
 
-use Dero\Data\DataException;
+use \Dero\Data\DataException;
 use \Dero\Data\Factory;
 use \Dero\Data\Parameter;
-use Dero\Data\ParameterCollection;
+use \Dero\Data\ParameterCollection;
 
 class BlogModel extends \Dero\Data\BaseModel
 {
+    const TABLE_NAME = 'blog_post';
+
+    private static $COLUMNS = [
+        'post_id' => [
+            'col_type' => COL_TYPE_INTEGER,
+            'required' => false,
+            'nullable' => false,
+            'key' => KEY_TYPE_PRIMARY
+        ],
+        'user_id' => [
+            'col_type' => COL_TYPE_INTEGER,
+            'required' => true,
+            'nullable' => false,
+            'key' => KEY_TYPE_FOREIGN
+        ],
+        'title' => [
+            'col_type' => COL_TYPE_TEXT,
+            'required' => true,
+            'nullable' => false,
+            'key' => null
+        ],
+        'body' => [
+            'col_type' => COL_TYPE_TEXT,
+            'required' => true,
+            'nullable' => false,
+            'key' => null
+        ],
+        'published' => [
+            'col_type' => COL_TYPE_BOOLEAN,
+            'required' => false,
+            'nullable' => false,
+            'key' => null
+        ],
+        'created' => [
+            'col_type' => COL_TYPE_DATETIME,
+            'required' => false,
+            'nullable' => false,
+            'key' => null
+        ],
+        'modified' => [
+            'col_type' => COL_TYPE_DATETIME,
+            'required' => false,
+            'nullable' => true,
+            'key' => null
+        ]
+    ];
+
     /**
      * Constructor
      */
@@ -34,7 +81,7 @@ class BlogModel extends \Dero\Data\BaseModel
     {
         $oParams = new ParameterCollection();
         $this->where(true);
-        $sql = 'SELECT * FROM ';
+        $sql = 'SELECT * FROM ' . self::TABLE_NAME;
         if( isset($aOpts['post_id']) )
         {
             $sql .= $this->where() . 'post_id = :post_id ';
@@ -54,7 +101,7 @@ class BlogModel extends \Dero\Data\BaseModel
 
         if( isset($aOpts['rows']) || isset($aOpts['skip']) )
         {
-            if(  isset($aOpts['rows']) || isset($aOpts['skip']) )
+            if(  isset($aOpts['rows']) && isset($aOpts['skip']) )
             {
                 $sql .= 'LIMIT :rows OFFSET :skip';
                 $oParams->Add(new Parameter('rows', $aOpts['rows']));
